ea9c132b2c7f7becf5d4e0e7ff50fedd6e7214e8
Vlad <vlad-labytin@yandex.ru>
2026-03-26 13:55:55 +0300

    3

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "store"
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls", namespace="store")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -6,3 +6,9 @@ def product_list(request):
     products = Product.objects.all()
     context = {"products":products}
     return render(request, "product_list.html", context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "products_detail.html", context)
