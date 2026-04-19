07f59ba39d66ff131c55ea1c5e0982a07b8108fd
Vlad <vlad-labytin@yandex.ru>
2026-04-19 11:06:37 +0300

    26.1 update

--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,31 +1,35 @@
 from django.urls import reverse_lazy
-
-from .models import Product
 from django.views.generic import ListView, DetailView
-from .forms import ProductForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .forms import ProductForm
+from .models import Product
 
 
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
 
+
 class ProductDetailView(DetailView):
     model = Product
     context_object_name = 'product'
 
+
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    success_url = ''
+    success_url = reverse_lazy("store:product_list")
+
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'store/product_form.html'
-    success_url = ''
+    success_url = reverse_lazy("store:product_list")
+
 
 class ProductDeleteView(DeleteView):
     model = Product
     template_name = 'store/product_confirm_delete.html'
-    success_url = ''
+    success_url = reverse_lazy("store:product_list")
