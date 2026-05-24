9d1642f9ff3d5390a97528fd4be31ab52ea63703
Vlad <vlad-labytin@yandex.ru>
2026-05-24 15:19:09 +0300

    1

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -141,3 +141,10 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 LOGIN_URL = 'users:login'
+
+CACHES = {
+    'default':{
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
